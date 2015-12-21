6c502bbce9e48420026f98c8712ab0e3638ffb75
aameer <aameer.rafiq@gmail.com>
2015-12-21 15:39:04 +0530

    chapter 3

--- functional_tests.py
+++ functional_tests.py
@@ -5,7 +5,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        #self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
 
         #She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the Test!')
+        self.fail('Finish the test!')
 
         #she is invited to enter a to-do item straight away
         
