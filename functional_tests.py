ab458f9d107e805a7bb316c218eb7b4f7364d549
aameer <aameer.rafiq@gmail.com>
2015-12-22 15:06:07 +0530

    add implicit wait to ft

--- functional_tests.py
+++ functional_tests.py
@@ -6,7 +6,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        #self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -18,8 +18,6 @@ class NewVisitorTest(unittest.TestCase):
 
         #She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-
-
         header = self.browser.find_element_by_tag_name('h1')
         self.assertIn('To-Do',header.text)
 
@@ -45,7 +43,7 @@ class NewVisitorTest(unittest.TestCase):
         #There is still  a text box inviting her to add another item. She 
         #enters "Use peacock feathers to make a fly" (Edith  is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
 
         table=self.browser.find_element_by_id('id_list_table')
@@ -55,10 +53,6 @@ class NewVisitorTest(unittest.TestCase):
                 [row.text for row in rows]
         )
 
-        self.assertIn(
-                "1: Buy peacock feathers",
-                [row.text for row in rows]
-        )
         self.fail('Finish the test!')
         #The page Updates again, and now shows both items on her list
         
